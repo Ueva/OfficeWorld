24f39a7893a3a7e02843385a8737e28fb4e7a250
Ueva <jbevans.cs@gmail.com>
2023-05-11 17:42:55 +0100

    Added Support for Re-Generating Office to UI (Press "R")

--- officeworld/interface/pygame_ui.py
+++ officeworld/interface/pygame_ui.py
@@ -1,6 +1,8 @@
+import json
 import pygame
 
 from officeworld.generator import CellType, OfficeGenerator
+from officeworld.utils.serialisation import EnumEncoder, as_enum
 
 Colors = {
     CellType.WALL: (50, 15, 15),
@@ -15,8 +17,20 @@ Colors = {
 }
 
 # Initialise Office Generator.
-# office_gen = OfficeGenerator(elevator_location=(7, 7))
-office_gen = OfficeGenerator(start_floor=0, goal_floor=0)
+num_floors = 1
+# office_gen = OfficeGenerator(num_floors=num_floors, elevator_location=(25, 20), start_floor=1, goal_floor=2)
+office_gen = OfficeGenerator(
+    floor_width=45,
+    floor_height=35,
+    min_room_area=9,
+    min_room_length=3,
+    max_hall_rate=0.2,
+    num_floors=num_floors,
+    elevator_location=(22, 17),
+    start_floor=0,
+    goal_floor=0,
+)
+# office_gen = OfficeGenerator(start_floor=0, goal_floor=0)
 # office = office_gen.generate_office_floor()
 
 # Display Variables.
@@ -38,14 +52,36 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("Courier New", 16)
 
+
+def regen_office():
+    office = office_gen.generate_office_building()
+    with open("office.json", "w") as f:
+        json.dump(office, f, cls=EnumEncoder)
+
+    with open("office.json", "r") as f:
+        office = json.load(f, object_hook=as_enum)
+
+    print(office_gen.generate_office_graph(layout=False).number_of_nodes())
+
+    return office
+
+
+office = regen_office()
+
+floor = 0
 running = True
 while running:
     # Process Events.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
-
-    office = office_gen.generate_office_building()[0]
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                floor = min(floor + 1, len(office) - 1)
+            elif event.key == pygame.K_DOWN:
+                floor = max(floor - 1, 0)
+            elif event.key == pygame.K_r:
+                office = regen_office()
 
     # Update Display.
     screen.fill(Colors[CellType.BACKGROUND])
@@ -54,12 +90,14 @@ while running:
         for x in range(office_gen.floor_width):
             pygame.draw.rect(
                 screen,
-                Colors[office[y][x]],
+                Colors[office[floor][y][x]],
                 pygame.Rect(OFFSET_X + x * BLOCK_SIZE, OFFSET_Y + y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE),
             )
 
     # Show FPS.
-    screen.blit(font.render(str(int(clock.get_fps())), 1, pygame.Color("WHITE")), (0, 0))
+    screen.blit(
+        font.render(f"{int(clock.get_fps())}fps   Floor {floor + 1} of {len(office)}", 1, pygame.Color("WHITE")), (0, 0)
+    )
 
     pygame.display.flip()
 
