7befdd4af6581139f05917ef4e26c86f8f812172
Joshua Evans <jbe25@bath.ac.uk>
2024-08-12 11:48:03 +0100

    Update serialisation.py
    - Added serialisation to .json for new OfficeBuilding class.

--- officeworld/utils/serialisation.py
+++ officeworld/utils/serialisation.py
@@ -1,6 +1,9 @@
 import json
 
-from officeworld.generator import CellType
+from enum import Enum
+
+from officeworld.generator.cell_type import CellType
+from officeworld.generator.office_building import OfficeBuilding
 
 PUBLIC_ENUMS = {"CellType": CellType}
 
@@ -12,10 +15,75 @@ class EnumEncoder(json.JSONEncoder):
             return {"__enum__": str(obj)}
         return json.JSONEncoder.default(self, obj)
 
+    @staticmethod
+    def decoder(obj):
+        if "__enum__" in obj:
+            name, member = obj["__enum__"].split(".")
+            return getattr(PUBLIC_ENUMS[name], member)
+        else:
+            return obj
 
-def as_enum(d):
-    if "__enum__" in d:
-        name, member = d["__enum__"].split(".")
-        return getattr(PUBLIC_ENUMS[name], member)
-    else:
-        return d
+
+class OfficeBuildingJSONHandler:
+    class Encoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, OfficeBuilding):
+                return {
+                    "__type__": "OfficeBuilding",
+                    "layout": obj.layout,
+                    "halls": obj.halls,
+                    "rooms": obj.rooms,
+                    "start_floor": obj.start_floor,
+                    "goal_floor": obj.goal_floor,
+                    "contains_start": obj.contains_start,
+                    "contains_goal": obj.contains_goal,
+                }
+            elif isinstance(obj, Enum):
+                return {"__enum__": str(obj)}
+            else:
+                return super().default(obj)
+
+    @staticmethod
+    def decoder(obj):
+        if "__type__" in obj and obj["__type__"] == "OfficeBuilding":
+            layout = obj["layout"]
+            decoded_layout = [
+                [
+                    [
+                        (
+                            getattr(CellType, cell["__enum__"].split(".")[1])
+                            if isinstance(cell, dict) and "__enum__" in cell
+                            else cell
+                        )
+                        for cell in row
+                    ]
+                    for row in floor
+                ]
+                for floor in layout
+            ]
+            decoded_halls = [[tuple(hall) for hall in floor] for floor in obj["halls"]]
+            decoded_rooms = [[tuple(room) for room in floor] for floor in obj["rooms"]]
+            return OfficeBuilding(
+                decoded_layout,
+                decoded_halls,
+                decoded_rooms,
+                obj["start_floor"],
+                obj["goal_floor"],
+                obj["contains_start"],
+                obj["contains_goal"],
+            )
+        elif "__enum__" in obj:
+            name, member = obj["__enum__"].split(".")
+            return getattr(PUBLIC_ENUMS[name], member)
+        else:
+            return obj
+
+    @staticmethod
+    def save_to_json(obj, file_path):
+        with open(file_path, "w") as f:
+            json.dump(obj, f, cls=OfficeBuildingJSONHandler.Encoder)
+
+    @staticmethod
+    def load_from_json(file_path):
+        with open(file_path, "r") as f:
+            return json.load(f, object_hook=OfficeBuildingJSONHandler.decoder)
